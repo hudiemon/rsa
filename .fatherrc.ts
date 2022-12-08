48c5cbaffdba9d33b98ef488a7a091e104ada6b4
liusize <895898088@qq.com>
2022-12-08 17:58:23 +0800

    :wrench: 同时支持浏览器与node环境

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'father';
+import {defineConfig} from 'father';
 
 export default defineConfig({
-  cjs: {},
-  prebundle: {},
+    esm: {},
+    cjs: {},
+    prebundle: {},
 });
